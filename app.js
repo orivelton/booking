17db85cdbf927614194b713b37e66545e4ac466c
Orivelton Cesar <orivelton10@gmail.com>
2020-07-20 15:48:24 +0100

    chore: Added GraphQL Query

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => ['Romantic Cooking', 'Sailing', 'All-Night Codding'],
+    createEvent: ({ name }) => name
+  }
+}));
 
 app.get('/', (req, res, next) => {
   res.send('>>>>>>>>>');
