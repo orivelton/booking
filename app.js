5518c992b75dee75c2d1d71eb4c9800330d3a040
Orivelton Cesar <orivelton10@gmail.com>
2020-07-20 17:53:21 +0100

    chore: Setup cloud storage

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-
 const app = express();
-const events = [];
+
+const Event = require('./models/event');
+
+const { MONGO_USER, MONGO_PASSWORD, MONGO_DB} = process.env;
 
 app.use(bodyParser.json());
 
@@ -40,24 +42,32 @@ app.use('/graphql', graphqlHTTP({
     }
   `),
   rootValue: {
-    events: () => events,
+    events: () => {
+      return Event.find().then(events => events.map(event => ({ ...event._doc, _id: event.id }))).catch(err => {
+        throw err;
+      })
+    },
     createEvent: ({ eventInput: { title, description, price, date }}) => {
-      const event = {
-        _id: Math.random().toString(),
+      const event = new Event({
         title,
         description,
         price: +price,
-        date
-      };
+        date: new Date(date)
+      });
 
-      events.push(event);
-      return event;
+      return event.save().then(result => {
+        console.log(result);
+        return {...result._doc, _id: result._doc._id.toString()};
+      }).catch(err => {
+        console.log(err);
+        throw err;
+      });
     }
   },
   graphiql: true
 }));
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ljjme.mongodb.net/test?retryWrites=true&w=majority`).then(() => {
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.ljjme.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`).then(() => {
   app.listen(3000);
 }).catch( err => {
   console.log(err);
