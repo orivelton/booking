cc91ef9f35bbc03ab79c737b5c20eb913605e44c
Orivelton Cesar <orivelton10@gmail.com>
2020-07-21 18:27:09 +0100

    refactor: app.js

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
-const app = express();
 
 const graphQlSchema = require('./graphql/schema');
 const graphQlResolvers = require('./graphql/resolvers');
-
-
 const { MONGO_USER, MONGO_PASSWORD, MONGO_DB} = process.env;
 
-
-
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -21,13 +17,8 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.ljjme.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`).then(() => {
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.ljjme.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true } ).then(() => {
   app.listen(3000);
 }).catch( err => {
   console.log(err);
 });
-
-
-
-
-
