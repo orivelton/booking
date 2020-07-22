47e01dfb78842ccf6e02ce35570cf814ac4bf8ea
Orivelton Cesar <orivelton10@gmail.com>
2020-07-22 15:52:42 +0100

    chore: Added booking model

--- models/booking.js
+++ models/booking.js
@@ -5,6 +5,14 @@ const bookingSchema = new Schema({
   event: {
     type: Schema.Types.ObjectId,
     ref: 'Event'
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
   }
+}, {
+  timestamps: true
 });
 
+
+module.exports = mongoose.model('Booking', bookingSchema)
