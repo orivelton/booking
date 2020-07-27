6c0a13b0d70bf7a2b185d30003a8b60676c3312b
Orivelton Cesar <orivelton10@gmail.com>
2020-07-27 16:36:20 +0100

    chore: Auth

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,10 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AuthPage = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  });
+
+  const requestBody = {
+    query: `
+      mutation {
+        createUser(
+          userInput: {
+            email: "${form.email}",
+            password: "${form.password}"
+          }
+        ) {
+          _id
+          email
+        }
+      }
+    `
+  };
+
+
+  const loginBody = {
+    query: `
+      query {
+        login(email: "${form.email}", password: "${form.password}") {
+          userId
+          token
+          tokenExpiration
+        }
+      }
+    `
+  };
+
+
+
+  const submitHandler = async e => {
+    e.preventDefault();
+    const isLogin = e.nativeEvent.submitter.id === 'login';
+
+    const body = isLogin ? loginBody : requestBody;
+
+    const result = await fetch('http://localhost:3000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).catch(err => { return err.json()});
+
+    console.log(result);
+  }
+
+  const updateField = e => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
   return(
-    <p>
-      Auth
-    </p>
+    <form onSubmit={submitHandler}>
+      <p>
+        <label htmlFor="email">
+          E-mail
+        </label>
+        <input 
+          type="email"
+          id="email"
+          name="email"
+          value={form.email}
+          onChange={updateField}
+          required 
+        />
+      </p>
+
+      <p>
+        <label htmlFor="password">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={form.password}
+          onChange={updateField}
+          required
+        />
+      </p>
+
+      <button type="submit" id="createAccount">
+        Create Account
+      </button>
+      <br />
+      <button type="submit" id="login">
+        Login
+      </button>
+    </form>
   )
 };
 
